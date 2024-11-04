8836ff19373b9f20dacaf4fcc1d0279fbff28180
Rishav0710 <rishavpandey.29.rp@gmail.com>
2024-11-04 16:13:15 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -13,6 +13,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig,model_trainer
+
 @dataclass 
 class Dataingestionconfig:
     train_data_path: str=os.path.join('artifacts','train.csv')
@@ -47,6 +49,12 @@ if __name__=='__main__':
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer=model_trainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+
+
+
 
 
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -28,7 +28,7 @@ class DataTransformation:
             categorical_columns=['gender', 'race_ethnicity', 'parental_level_of_education', 'lunch', 'test_preparation_course']
 
             num_pipeline=Pipeline(steps=[('imputer',SimpleImputer(strategy='median')),
-                                         ('scaler',StandardScaler(with_mean='False'))
+                                         ('scaler',StandardScaler(with_mean=False))
                                          ])
             cat_pipeline=Pipeline(steps=[('imputer',SimpleImputer(strategy='most_frequent')),
                                          ('one_hot_encoder',OneHotEncoder(handle_unknown='ignore')),
@@ -64,7 +64,7 @@ class DataTransformation:
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
 
-            input_feature_test_df=test_df.drop(column=[target_column_name],axis=1)
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
             target_feature_test_df=test_df[target_column_name]
 
             logging.info("Applying preprocessing object on training and testing dataframe")
